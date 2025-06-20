5202fb7dbfeed4f0ab4ff7210f77cbdce0cb3f7f
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-06-20 12:55:43 +0300

    add component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,51 @@
 import './Sidebar.css';
+import type { MenuItemsProps } from '../../types/MenuItemsProps';
 
 const Sidebar = () => {
+
+  const menuItems: MenuItemsProps[] = [
+    {
+      label: "Dashboard",
+      link: "#",
+      icon: "fa-house"
+    },
+    {
+      label: "Calendar",
+      link: "#",
+      icon: "fa-calendar"
+    },
+    {
+      label: "Event",
+      link: "#",
+      icon: "fa-bolt-lightning"
+    },
+    {
+      label: "Analytics",
+      link: "#",
+      icon: "fa-chart-pie"
+    },
+    {
+      label: "Kanban",
+      link: "#",
+      icon: "fa-table-cells-large"
+    },
+  ]
+
   return (
     <section className="sidebar">
+      <h4 className="sidebarTitle">Main menu</h4>
       <nav>
-        <div className="mainMenu">
-          <h4>Main menu</h4>
-        </div>
+        <ul className="mainMenu">
+          {menuItems.map(({ label, link, icon }, key) => (
+            <li key={key} className="menuItems">
+              <i className={`fa-solid ${icon}`}></i>
+              <a href={link} className="h4">{label}</a>
+            </li>
+          ))}
+        </ul>
       </nav>
-    </section>
+    </section >
   );
 }
 export default Sidebar;
+
---
+++ src/types/MenuItemsProps.ts
@@ -0,0 +1,5 @@
+export interface MenuItemsProps{
+  label:string;
+  link: string;
+  icon?:string;
+}
